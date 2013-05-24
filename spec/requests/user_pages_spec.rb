33c41d168b09d6b5dae172b7dd4d00d19e7550cc
Veronika Wu <vero.hecko@gmail.com>
2013-05-24 16:56:58 -0700

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -29,6 +29,22 @@ describe "signup" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end
+      describe "display error when password is too short" do
+        before do
+          fill_in "Name",         with: "Example User"
+          fill_in "Email",        with: "user@example.com"
+          fill_in "Password",     with: "bar"
+          fill_in "Confirmation", with: "bar"
+          click_button submit
+        end
+        it { should have_content('too short') }
+      end
     end
 
     describe "with valid information" do
@@ -37,6 +53,15 @@ describe "signup" do
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
 
       it "should create a user" do
